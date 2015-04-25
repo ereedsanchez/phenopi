ccde402ac0976a915760bdc55ccdddbd0287e1d2
Koen Hufkens <khufkens@Pandora.local>
2015-04-25 15:48:07 -0400

    added mjpeg install routine

---
+++ install_mjpeg_daemon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# install the mjpeg streamer
+# for an active view of the camera
+
+# install necessary libraries
+sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
+sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+
+# download the mjpeg streamer daemon
+# and unzip the file
+wget http://sourceforge.net/code-snapshots/svn/m/mj/mjpg-streamer/code/mjpg-streamer-code-182.zip
+unzip mjpg-streamer-code-182.zip
+
+# enter the unzipped directory and compile the
+# necessary parts
+cd mjpg-streamer-code-182/mjpg-streamer
+make mjpg_streamer input_file.so output_http.so
+
+# copy all parts to the necessary locations
+sudo cp mjpg_streamer /usr/local/bin
+sudo cp output_http.so input_file.so /usr/local/lib/
+sudo cp -R www /usr/local/www
+
+# clean up the compilation directory
+cd ../../
+rm -rf mjpg-streamer-182
+
+#LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"
