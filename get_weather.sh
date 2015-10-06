f77d0595808e2a85f47e9ff9ac334e9a1c4fff75
Koen Hufkens <khufkens@Pandora.local>
2015-10-06 12:20:45 -0400

    added geoip lookup script

--- get_weather.sh
+++ get_weather.sh
@@ -15,8 +15,8 @@ function get_weather() {
 		geolocation_data=$(./pigeoip.py ${current_ip})
 
 		# look up the location based upon the external ip
-		latitude=$(echo ${geolocation_data} | awk '{print $1}'
-		longitude=$(echo ${geolocation_data} | awk '{print $2}'
+		latitude=$(echo ${geolocation_data} | awk '{print $1}')
+		longitude=$(echo ${geolocation_data} | awk '{print $2}')
 
 		# get the current weather for the current location
 		# for some reason I can't split the lines, keep as is
